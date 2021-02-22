30ca03586fa8f4637f13894740bafe5663a99b2f
Mahamadou <kmahamadou01@gmail.com>
2021-02-22 17:08:39 +0000

    Layout Fixed

--- app/Http/Controllers/PasswordResetController.php
+++ app/Http/Controllers/PasswordResetController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\PasswordReset;
 
 class PasswordResetController extends Controller
 {
     //
+
+    public function resetter($query)
+    {
+    	$phone 		= substr($query, start);
+    }
 }
